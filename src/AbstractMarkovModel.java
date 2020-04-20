9b919370164369f231d6b48010a1f8d8f5f7955e
Hamad <hammadkhan@live.com>
2020-04-21 00:18:35 +0100

    Markov Model with Interface

---
+++ src/AbstractMarkovModel.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public abstract class AbstractMarkovModel implements IMarkovModel {
+    protected String myText;
+    protected Random myRandom;
+
+    public AbstractMarkovModel() {
+        myRandom = new Random();
+    }
+
+    public void setTraining(String s) {
+        myText = s.trim();
+    }
+
+    abstract public String getRandomText(int numChars);
+
+    protected ArrayList<String> getFollows(String key) {
+        ArrayList<String> follows = new ArrayList<String>();
+        for (int i = 0; i < myText.length() - key.length(); i++) {
+            if (myText.substring(i, i + key.length()).equals(key)) {
+                follows.add(myText.substring(i + key.length(), i + 1 + key.length()));
+            }
+        }
+        return follows;
+    }
+
+}
